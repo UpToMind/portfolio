91020f161e2baa55e4db9fc1d599f3d0590a432a
이동준 <lee19980424@gmail.com>
2024-11-02 21:43:55 +0900

    첫 화면 보이는 부분 만듦

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   Splitting();
 });
+
+// 헤더 영역 스크롤 이벤트
+document.addEventListener("DOMContentLoaded", function () {
+  let prevScrollTop = 0;
+
+  document.addEventListener("scroll", function () {
+    let nowScrollTop = window.scrollY;
+    const header = document.querySelector("header");
+
+    if (nowScrollTop > prevScrollTop) {
+      header.classList.add("active");
+    } else {
+      header.classList.remove("active");
+    }
+    prevScrollTop = nowScrollTop;
+  });
+});
